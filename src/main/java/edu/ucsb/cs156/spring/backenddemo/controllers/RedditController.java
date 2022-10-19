30a53f65e3da6c94cda195f88fd03b5d4070ff29
Ethan Mader <thoma@169-231-126-178.wireless.ucsb.edu>
2022-10-18 18:12:39 -0700

    em - started RedditController

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/RedditController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/RedditController.java
@@ -2,9 +2,31 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.RedditQueryService;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(description="Subreddits info from your mom")
+@Slf4j
 @RestController
+@RequestMapping("/api/subreddits")
 public class RedditController {
     
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    RedditQueryService redditQueryService;
 
 }
