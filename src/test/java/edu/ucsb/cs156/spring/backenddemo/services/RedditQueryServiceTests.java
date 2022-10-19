81a97304fb84a203d9ddcc5c4a00d6da35723bd2
Ethan Mader <thoma@169-231-126-178.wireless.ucsb.edu>
2022-10-18 18:05:50 -0700

    em - RedditQueryServiceTests passing

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/services/RedditQueryServiceTests.java
@@ -2,7 +2,6 @@ package edu.ucsb.cs156.spring.backenddemo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
 
@@ -16,13 +15,8 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestClientTest(RedditQueryService.class)
 public class RedditQueryServiceTests {
@@ -42,9 +36,9 @@ public class RedditQueryServiceTests {
         String fakeJsonResult = "{ \"fake\" : \"result\" }";
 
         this.mockRestServiceServer.expect(requestTo(expectedURL))
-                .andExpect(header("Accept", "text/plain"))
+                .andExpect(header("Accept", MediaType.APPLICATION_JSON.toString()))
                 .andExpect(header("Content-Type", MediaType.APPLICATION_JSON.toString()))
-                .andRespond(withSuccess(fakeJsonResult, MediaType.TEXT_PLAIN));
+                .andRespond(withSuccess(fakeJsonResult, MediaType.APPLICATION_JSON));
 
         String actualResult = redditQueryService.getJSON(subreddit);
         assertEquals(fakeJsonResult, actualResult);
