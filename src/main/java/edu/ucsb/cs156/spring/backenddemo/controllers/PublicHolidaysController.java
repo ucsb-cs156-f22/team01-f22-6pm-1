0506dd9f77e304f13ec25d52e9cacf5a2ef05008
Saahil Joshi <saahiljoshi@ucsb.edu>
2022-10-19 20:15:14 -0700

    sj- added body from earthquake controller, still needs some replacement

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
@@ -2,7 +2,7 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.ucsb.cs156.spring.backenddemo.services.EarthquakeQueryService;
+import edu.ucsb.cs156.spring.backenddemo.services.PublicHolidayQueryService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +19,26 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 
+@Api(description="Public Holiday info from") //FILL THIS IN 
+@Slf4j
 @RestController
+@RequestMapping("/api/publicholidays")
 public class PublicHolidaysController {
     
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    PublicHolidayQueryService publicHolidayQueryService;
+
+    @ApiOperation(value = "Get earthquakes a certain distance from UCSB's Storke Tower", notes = "JSON return format documented here: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php")
+    @GetMapping("/get")
+    public ResponseEntity<String> getEarthquakes(
+        @ApiParam("distance in km, e.g. 100") @RequestParam String distance,
+        @ApiParam("minimum magnitude, e.g. 2.5") @RequestParam String minMag
+    ) throws JsonProcessingException {
+        log.info("getEarthquakes: distance={} minMag={}", distance, minMag);
+        String result = earthquakeQueryService.getJSON(distance, minMag);
+        return ResponseEntity.ok().body(result);
+    }
+
 }
